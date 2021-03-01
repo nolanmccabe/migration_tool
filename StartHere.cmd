740ea133bc88bac2eda57700df900ca7972e062b
mccabeno <34149709+nolanmccabe@users.noreply.github.com>
2021-03-01 08:35:24 -0800

    Update StartHere.cmd

--- StartHere.cmd
+++ StartHere.cmd
@@ -1,6 +1,6 @@
 @echo off
 Color 02
-If exist "D:\migration_tool\" goto StartTeacherMigrationTool
+If exist "D:\migration_tool*\" goto StartTeacherMigrationTool
 :StartTeacherMigrationTool
 powershell.exe -executionpolicy unrestricted -noprofile -WindowStyle Maximized -command "&{start-process powershell -ArgumentList '-executionpolicy unrestricted -noprofile -WindowStyle Maximized -file D:\TeacherMigrationTool\TeacherMigrationTool.ps1' -verb RunAs}"
 exit
